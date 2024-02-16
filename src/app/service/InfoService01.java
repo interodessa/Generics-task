5353cf3c8802296206f165518f406de6d9e7804f
interodessa <alexkotuz@gmail.com>
2024-02-16 18:10:17 +0200

    Initial commit

---
+++ src/app/service/InfoService01.java
@@ -0,0 +1,32 @@
+package app.service;
+
+import app.entity.Client;
+
+import java.util.Scanner;
+
+public class InfoService01 {
+
+    Scanner scanner;
+    String name;
+    String email;
+
+    public String getData() {
+
+        String s = formData(getInputs());
+        return s;
+    }
+
+    private Client<String, String> getInputs() {
+        scanner = new Scanner(System.in);
+        System.out.print("Enter client's name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter client's email: ");
+        email = scanner.nextLine();
+        return new Client (name, email);
+    }
+
+    private String formData(Client<String, String> client) {
+        return "Client's name is " + client.getKey() +
+                ", email is " + client.getValue();
+    }
+}
